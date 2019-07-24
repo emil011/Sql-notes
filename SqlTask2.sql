6dc2e531cb0dd302c5385ad1bdfe436a2dae306a
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-24 14:59:54 +0400

    Update SqlTask2.sql

--- SqlTask2.sql
+++ SqlTask2.sql
@@ -1,7 +1,6 @@
 -- Display the departments into which no employee joined in last two years.
 
 select d.department_id, d.department_name, count(e.employee_id) as employee 
-
     from employees e 
     right join departments d on d.department_id = e.department_id 
     and extract (year from e.hire_date) >= (extract(year from sysdate) - 2)
