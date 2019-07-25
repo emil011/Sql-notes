211498b1d5fb8e594691f31b3a2b0cfb0f57c3ce
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-25 10:49:00 +0400

    Create SqlTask5.sql

---
+++ SqlTask5.sql
@@ -0,0 +1,7 @@
+-- Display department name, manager name, and salary of the manager for all managers whose experience is more than 5 years.
+
+select d.department_name,e.first_name,e.salary
+    from hr.employees e 
+    join hr.departments d on e.manager_id = d.manager_id
+    where months_between(sysdate,e.hire_date) > 5
+/
