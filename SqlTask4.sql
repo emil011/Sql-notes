023b1d95ee77436185cf9a816ef62213cf6ae6b0
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-29 16:17:37 +0400

    Update SqlTask4.sql

--- SqlTask4.sql
+++ SqlTask4.sql
@@ -1,9 +1,11 @@
 -- Display departments where any manager is managing more than 5 employees.
 
-select d.department_id,d.department_name,count(e.employee_id) as employees
-    from hr.employees e 
-    join hr.departments d on e.manager_id = d.manager_id
-    group by d.department_id,d.department_name
+select emp.department_id , dep.department_name,count(e.employee_id) as employees
+    from employees e 
+    join employees emp on e.manager_id = emp.employee_id
+    join departments dep on emp.department_id = dep.department_id
+    group by emp.department_id,dep.department_name
     having count(e.employee_id) > 5
+    order by emp.department_id asc
  
 /
