4a2d0ede2473c0f154a454c702c2b9cad3f9a713
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-29 17:48:55 +0400

    Create SqlTask8.sql

---
+++ SqlTask8.sql
@@ -0,0 +1,10 @@
+-- Write a query in SQL to display the country name, city, and number of those departments where at least 2 employees are working.
+
+
+select c.country_name,loc.city, count(distinct d.department_id)
+from employees e join departments d on e.department_id = d.department_id
+join locations loc on loc.location_id = d.location_id
+join countries c on c.country_id = loc.country_id
+group by loc.city,c.country_name
+having count(e.employee_id) >= 2
+/
