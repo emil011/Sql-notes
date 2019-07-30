ade73040069efc52e109e7da7efc67ff21e9844e
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-30 20:49:15 +0400

    Update SqlTask19.sql

--- SqlTask19.sql
+++ SqlTask19.sql
@@ -4,5 +4,4 @@ select extract(month from e.hire_date) as month,count(e.employee_id) as employee
 from employees e
 where extract(year from e.hire_date) = extract(year from sysdate)
 group by extract(month from e.hire_date)
-
 /
