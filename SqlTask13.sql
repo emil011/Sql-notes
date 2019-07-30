740c25ad1b54af0000235f46727032749eeff4be
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-30 20:32:38 +0400

    Create SqlTask13.sql

---
+++ SqlTask13.sql
@@ -0,0 +1,7 @@
+-- Display employee name, job title for the jobs employee did in the past where the job was done less than six months.
+
+select e.first_name,e.last_name,j.job_title,round(months_between(jh.end_date,jh.start_date),2)
+from employees e join jobs j on j.job_id = e.job_id
+join job_history jh on jh.employee_id = e.employee_id
+where round(months_between(jh.end_date,jh.start_date),2) < 6
+/
