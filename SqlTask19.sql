9fca49fe7bf8c4a5b4d863e970e32804549279f8
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-30 20:48:51 +0400

    Create SqlTask19.sql

---
+++ SqlTask19.sql
@@ -0,0 +1,8 @@
+-- Display how many employees joined in each month of the current year.
+
+select extract(month from e.hire_date) as month,count(e.employee_id) as employees
+from employees e
+where extract(year from e.hire_date) = extract(year from sysdate)
+group by extract(month from e.hire_date)
+
+/
